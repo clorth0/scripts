1f06e218f4fef98b0ffe03ce7452d39f46946467
Matt James <6087477+clorth0@users.noreply.github.com>
2023-05-22 14:11:03 -0500

    Update check-headers.py

--- check-headers.py
+++ check-headers.py
@@ -1,19 +1,19 @@
 import requests
 from colorama import Fore, init
 
-SECURITY_HEADERS = [
-    'Strict-Transport-Security',
-    'X-Frame-Options',
-    'X-Content-Type-Options',
-    'Content-Security-Policy',
-    'X-Permitted-Cross-Domain-Policies',
-    'Referrer-Policy',
-    'Clear-Site-Data',
-    'Cross-Origin-Embedder-Policy',
-    'Cross-Origin-Opener-Policy',
-    'Cross-Origin-Resource-Policy',
-    'Cache-Control',
-]
+SECURITY_HEADERS = {
+    'Strict-Transport-Security': "Missing this header can expose the website to downgrade attacks, SSL stripping and cookie hijacking.",
+    'X-Frame-Options': "Without this header, your website could be at risk of clickjacking attacks.",
+    'X-Content-Type-Options': "Missing this header can lead to attacks such as MIME sniffing.",
+    'Content-Security-Policy': "If this header is missing, your website is more susceptible to cross-site scripting (XSS) attacks.",
+    'X-Permitted-Cross-Domain-Policies': "Missing this header can allow data loading from unknown sources, potentially leading to data theft.",
+    'Referrer-Policy': "Without this header, browsers can send full URL of the previous page leading to possible data leakage.",
+    'Clear-Site-Data': "Missing this header could allow data to persist in the browser, leading to data leakage.",
+    'Cross-Origin-Embedder-Policy': "Without this header, the website may be vulnerable to attacks involving loading of resources.",
+    'Cross-Origin-Opener-Policy': "Without this header, your website could be at risk of tab-nabbing attacks.",
+    'Cross-Origin-Resource-Policy': "Missing this header can lead to unauthorized loading of resources.",
+    'Cache-Control': "Without proper cache control, sensitive data can be stored by intermediaries or browser caches."
+}
 
 def check_headers(target):
     try:
@@ -24,12 +24,18 @@ def check_headers(target):
         return
     
     headers = response.headers
-    for header in SECURITY_HEADERS:
+    print(Fore.GREEN + "Full headers: ")
+    for h, v in headers.items():
+        print(Fore.GREEN + f"{h}: {v}")
+    print(Fore.RESET)
+
+    for header, risk in SECURITY_HEADERS.items():
         print(Fore.GREEN + header, end=": ")
         if header in headers:
             print(Fore.GREEN + "Present")
         else:
             print(Fore.RED + "Missing")
+            print(Fore.RED + "Risk: ", risk)
 
 if __name__ == "__main__":
     init(autoreset=True)  # initialize colorama
